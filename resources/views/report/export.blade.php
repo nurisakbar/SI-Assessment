33cc256185ae111f3f94f33df95094b487c2883f
Wahyu Safrizal <49548492+wahyusafrizal17@users.noreply.github.com>
2025-01-20 21:23:01 +0700

    update pisahkan pertanyaan dan jawaban

--- resources/views/report/export.blade.php
+++ resources/views/report/export.blade.php
@@ -3,46 +3,86 @@
         <tr>
             <th style="background-color: #0D9488; color: white;">No</th>
             <th style="background-color: #0D9488; color: white;">Nama</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 1 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 2 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 3 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 4 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 5 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 6 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 7 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 8 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 9 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 10 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 11 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 12 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 13 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 14 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 15 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 16 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 17 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 18 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 19 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 20 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 21 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 22 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 23 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 24 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 25 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 26 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 27 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 28 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 29 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 30 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 31 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 32 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 33 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 34 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 35 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 36 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 37 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 38 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 39 | Jawaban</th>
-            <th style="background-color: #0D9488; color: white;">Pertanyaan 40 | Jawaban</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 1</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 1 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 2</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 2 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 3</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 3 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 4</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 4 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 5</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 5 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 6</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 6 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 7</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 7 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 8</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 8 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 9</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 9 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 10</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 10 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 11</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 11 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 12</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 12 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 13</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 13 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 14</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 14 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 15</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 15 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 16</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 16 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 17</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 17 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 18</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 18 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 19</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 19 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 20</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 20 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 21</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 21 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 22</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 22 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 23</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 23 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 24</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 24 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 25</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 25 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 26</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 26 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 27</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 27 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 28</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 28 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 29</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 29 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 30</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 30 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 31</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 31 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 32</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 32 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 33</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 33 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 34</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 34 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 35</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 35 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 36</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 36 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 37</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 37 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 38</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 38 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 39</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 39 (Poin)</th>
+            <th style="background-color: #0D9488; color: white;">Pertanyaan 40</th>
+            <th style="background-color: #0D9488; color: white;">Jawaban 40 (Poin)</th>
         </tr>
      </thead>
      <tbody>
@@ -52,13 +92,26 @@
           <td>{{ $row->name}}</td>
           <?php
            $pertanyaan = \DB::table('pertanyaan as p')
-               ->join('jawaban as j', 'j.pertanyaan_id', '=', 'p.id')
-               ->where('j.user_id', $row->id)
-               ->select(\DB::raw("CONCAT(p.pertanyaan, ' | ', j.jawaban) AS pertanyaan_jawaban"))
-               ->get();
+                        ->join('jawaban as j', 'j.pertanyaan_id', '=', 'p.id')
+                        ->where('j.user_id', $row->id)
+                        ->select('p.pertanyaan', 'j.jawaban')
+                        ->get();
           ?>
           @foreach($pertanyaan as $p)
-           <td>{{ $p->pertanyaan_jawaban }}</td>
+            <?php
+                $label_jawaban = 'Tidak pernah';
+                if($p->jawaban == 2){
+                    $label_jawaban = 'Jarang';
+                }elseif($p->jawaban == 3){
+                    $label_jawaban = 'Kadang-kadang';
+                }elseif($p->jawaban == 4){
+                    $label_jawaban = 'Sering';
+                }else{
+                    $label_jawaban = 'Sangat sering';
+                }
+            ?>
+           <td>{{ $p->pertanyaan }}</td>
+           <td>{{ $label_jawaban }} ({{ $p->jawaban }})</td>
           @endforeach
        </tr>
        @endforeach
